02f8f48340b909bdb9313bb0faf9e2f5dc2d1d0d
adjoaedwin <aedwin905@gmail.com>
2018-06-27 21:47:21 +0000

    Added counter schema

--- models/url.js
+++ models/url.js
@@ -2,32 +2,28 @@
 
 const mongoose = require('mongoose')
 
-const counterSchema = new mongoose.Schema({
-    _id : {type: String, required: true},
-    count: {type: Number, default: 0}
+const counterSchema = mongoose.Schema({
+    _id: {type: String, required: true},
+    seq: {type: Number, default: 0}
 })
 
-const Counter = module.exports = mongoose.model('Counter', counterSchema)
+const counter = module.exports = mongoose.model('counter', counterSchema)
 
-
-const urlSchema = new mongoose.model({
-    _id : {type: String},
-    url : '',
-    created_at : '' 
+const urlSchema = new mongoose.Schema({
+    _id: {type: Number, index: true, required: true},
+    created_at : {type: Date, default: Date.now()},
+    long_url: {type: String, required: true}
 })
 
 urlSchema.pre('save', (next) => {
-    console.log('Running pre-save')
-    let doc = this
-    Counter.findByIdAndUpdate({_id: 'url_count'}, { $inc: {count: 1}}, (err, counter) => {
-        if(err) return next(err)
-        console.log(counter)
-        console.log(counter.count)
-        doc._id = counter.count
-        doc.created_at = new Date
-        console.log(doc)
+    let doc: this
+    counter.findByIdAndUpdate({ _id: 'url_count'}, { $inc: { seq: 1} }, (err, counter) => {
+        if (err) 
+            return next(err)
+        doc._id = counter.seq
+        doc.created_at = Date.now()
         next()
     })
 })
 
-const URL = module.exports = mongoose.model('URL', urlSchema)
+const Url = module.exports = mongoose.model('URL', urlSchema)
